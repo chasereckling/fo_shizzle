17019b23b4be4446f35754d1385f5ad1fd350a5d
jessica engel & chase reckling <student@epicodus.com>
2015-04-22 11:16:46 -0700

    add views folder, index.erb, created form

--- lib/fo_shizzle_integration_spec.rb
+++ lib/fo_shizzle_integration_spec.rb
@@ -6,7 +6,7 @@ describe('the fo_shizzle path', {:type => :feature}) do
   it('processes the user entry and returns it in snoop dogg speak') do
     visit('/')
     fill_in('sentence',:with => 'This is Sallys sentence')
-    click_button('Send')
+    click_button('Holla at your boi')
     expect(page).to have_content('Thiz iz Sallyz sentence')
   end
 end
